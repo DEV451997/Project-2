4f09cd25028638d025ca203a1a7d89368c401beb
Arron Evans <aredev45@gmail.com>
2023-07-12 16:51:41 +0000

    created a clock function to show the local time in italy. I also sized up the navbar and arranged the tiles to be opposite of eachover

--- java.js
+++ java.js
@@ -1,3 +1,5 @@
+// Slideshow of images
+
 var images = ["/assets/images/ROME.jpg", "/assets/images/MEXICO.jpg", "/assets/images/JORDAN.jpg", "/assets/images/CHINA.jpg", "/assets/images/BRAZIL.jpg", "/assets/images/PERU.jpg", "/assets/images/INDIA.jpg"];
 var currentIndex = 0;
 var slideshowCard = document.querySelector(".slideshow-card");
@@ -7,7 +9,7 @@ function showSlide(index) {
     setTimeout(function () {
         slideshowCard.innerHTML = '<img src="' + images[index] + '" alt="Image ' + (index + 1) + '">';
         slideshowCard.style.opacity = 1;
-    }, 500); // Transition duration
+    }, 500); 
 }
 
 function nextSlide() {
@@ -19,6 +21,25 @@ function startSlideshow() {
     nextSlide();
 }
 
-setInterval(startSlideshow, 5000); // Transition every 3 seconds
+setInterval(startSlideshow, 5000); 
 
 showSlide(currentIndex);
+
+// Clock function
+
+function startTime() {
+    const today = new Date();
+    const options = {
+        timeZone: 'Europe/Rome', // Specify the desired timezone here
+        hour12: true, // Set to true for 12-hour format, false for 24-hour format
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+    const timeString = today.toLocaleTimeString('en-US', options);
+
+    document.getElementById("clock").innerHTML = timeString;
+    setTimeout(startTime, 1000);
+}
+
+startTime();
